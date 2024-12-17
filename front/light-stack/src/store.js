74778f68e74c4102967fc42f3594bdca29b45a3c
Hajar BAKAROU <hajar.bakarou@emsi-edu.ma>
2024-12-17 14:00:15 +0000

    Q7 & Q8 Q9 & Q10 & Q11

--- front/light-stack/src/store.js
+++ front/light-stack/src/store.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://url.api";
+export const baseUrl = "https://8080-bakarouhaja-miagenumres-vvv1hy4orel.ws-eu117.gitpod.io";
 
 export function getQuestions() {
   let questions;
